6890c1f26b868b85b000c5c451a1c13d53f1dc8f
Akram Hassan <akram.imam82@gmail.com>
2018-06-12 13:13:25 +0200

    Cleanup

--- google-maps-ios-sample/google-maps-ios-sample/ViewController.swift
+++ google-maps-ios-sample/google-maps-ios-sample/ViewController.swift
@@ -8,11 +8,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -30,7 +26,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         if CLLocationManager.locationServicesEnabled() {
             locationManager.startUpdatingLocation()
-            //locationManager.startUpdatingHeading()
         }
     }
     
